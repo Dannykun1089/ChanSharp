884dffbcba73d8128231bcefa2aff14f3da2db8e
DannyK <Dannykun1089@protonmail.com>
2021-08-01 14:57:33 +0100

    Touch up the UrlGenerator class, fixed impropper static properties

--- src/UrlGenerator.cs
+++ src/UrlGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 
 namespace ChanSharp
 {
@@ -9,14 +8,14 @@ namespace ChanSharp
         ///   Properties   ///
         //////////////////////
 
-        private static string  BoardName        { get; set; }
-        private static string  Protocol         { get; set; }
+        private string BoardName { get; set; }
+        private string Protocol { get; set; }
 
-        private static JObject Urls             { get; set; }
+        private JToken Domain { get => Domain_get(); }
+        private JToken Template { get => Template_get(); }
+        private JToken Listing { get => Listing_get(); }
 
-        private static JToken  Domain           { get; set; }
-        private static JToken  Template         { get; set; }
-        private static JToken  Listing          { get; set; }
+        private JObject Urls { get => Urls_get(); }
 
 
 
@@ -28,43 +27,6 @@ namespace ChanSharp
         {
             BoardName = boardName;
             Protocol = https ? "https://" : "http://";
-
-            Domain = JToken.Parse($@"{{
-                                        'api':             '{ Protocol }a.4cdn.org',
-                                        'boards':          '{ Protocol }boards.4chan.org' ,
-                                        'boards_4channel': '{ Protocol }boards.4channel.org',
-                                        'file':            '{ Protocol }i.4cdn.org',
-                                        'thumbs':          '{ Protocol }i.4cdn.org',
-                                        'static':          '{ Protocol }s.4cdn.org'
-                                    }}");
-
-            Template = JToken.Parse($@"{{
-                                          'api':  {{
-                                                     'board':  '{ Domain["api"] }/{{0}}/{{1}}.json',
-                                                     'thread': '{ Domain["api"] }/{{0}}/thread/{{1}}.json'
-                                                  }},
-                                          'http': {{
-                                                     'board':  '{ Domain["api"] }/{{0}}/{{1}}',
-                                                     'thread': '{ Domain["api"] }/{{0}}/thread/{{1}}'
-                                                  }},
-                                          'data': {{
-                                                      'file':   '{ Domain["file"] }/{{0}}/{{1}}{{2}}',
-                                                      'thumbs': '{ Domain["thunbs"] }/{{0}}/{{1}}s.jpg',
-                                                      'static': '{ Domain["static"] }/image/{{0}}'
-                                                  }}
-                                       }}");
-
-            Listing = JToken.Parse($@"{{
-                                           'board_list':           '{ Domain["api"] }/boards.json',
-                                           'thread_list':          '{ Domain["api"] }/{{0}}/threads.json',
-                                           'archived_thread_list': '{ Domain["api"] }/{{0}}/archive.json',
-                                           'catalog':              '{ Domain["api"] }/{{0}}/catalog.json'
-                                      }}");
-
-
-            Urls = JObject.FromObject(Template);
-            Urls.Add("domain", Domain);
-            Urls.Add("listing", Listing);
         }
 
 
@@ -75,55 +37,126 @@ namespace ChanSharp
 
         public string BoardList()
         {
-            return (string) Urls["listing"]["board_list"];
+            return Urls["listing"].Value<string>("board_list");
         }
 
 
         public string PageUrls(int page)
         {
-            return string.Format((string) Urls["api"]["board"], BoardName, page);
+            return string.Format(Urls["api"].Value<string>("board"),
+                                  BoardName,
+                                  page);
         }
 
 
         public string Catalog()
         {
-            return string.Format((string) Urls["listing"]["catalog"], BoardName);
+            return string.Format(Urls["listing"].Value<string>("catalog"),
+                                  BoardName);
         }
 
 
         public string ThreadList()
         {
-            return string.Format((string)Urls["listing"]["thread_list"], BoardName);
+            return string.Format(Urls["listing"].Value<string>("thread_list"),
+                                  BoardName);
         }
 
 
         public string ThreadAPIUrls(int threadID)
         {
-            return string.Format((string)Urls["api"]["thread"], BoardName, threadID);
+            return string.Format(Urls["api"].Value<string>("thread"),
+                                  BoardName,
+                                  threadID);
         }
 
 
         public string ThreadUrls(string threadID)
         {
-           return string.Format((string)Urls["http"]["thread"], BoardName, threadID);
+            return string.Format(Urls["http"].Value<string>("thread"),
+                                  BoardName,
+                                  threadID);
         }
 
 
         public string FileUrls(string tim, string ext)
         {
-           return string.Format((string)Urls["data"]["file"], BoardName, tim, ext);
+            return string.Format(Urls["data"].Value<string>("file"),
+                                  BoardName,
+                                  tim,
+                                  ext);
         }
 
 
         public string ThumbUrls(string tim)
         {
-            return string.Format((string)Urls["data"]["thumbs"], tim);
+            return string.Format(Urls["data"].Value<string>("thumbs"),
+                                  tim);
         }
 
 
-        public static JObject SiteUrls()
+        public JObject SiteUrls()
         {
             return Urls;
+        }
+
+
+
+        /////////////////////////////////
+        ///   Property get; Methods   ///
+        /////////////////////////////////
+
+        private JToken Domain_get()
+        {
+            return JToken.Parse($@"{{
+                                        'api':             '{ Protocol }a.4cdn.org',
+                                        'boards':          '{ Protocol }boards.4chan.org' ,
+                                        'boards_4channel': '{ Protocol }boards.4channel.org',
+                                        'file':            '{ Protocol }i.4cdn.org',
+                                        'thumbs':          '{ Protocol }i.4cdn.org',
+                                        'static':          '{ Protocol }s.4cdn.org'
+                                   }}");
+        }
+
+
+        private JToken Template_get()
+        {
+            return JToken.Parse($@"{{
+                                        'api':  {{
+                                                     'board':  '{ Domain["api"] }/{{0}}/{{1}}.json',
+                                                     'thread': '{ Domain["api"] }/{{0}}/thread/{{1}}.json'
+                                                }},
+                                        'http': {{
+                                                     'board':  '{ Domain["api"] }/{{0}}/{{1}}',
+                                                     'thread': '{ Domain["api"] }/{{0}}/thread/{{1}}'
+                                                }},
+                                        'data': {{
+                                                     'file':   '{ Domain["file"] }/{{0}}/{{1}}{{2}}',
+                                                     'thumbs': '{ Domain["thunbs"] }/{{0}}/{{1}}s.jpg',
+                                                     'static': '{ Domain["static"] }/image/{{0}}'
+                                                }}
+                                   }}");
+        }
+
+
+        private JToken Listing_get()
+        {
+            return JToken.Parse($@"{{
+                                        'board_list':           '{ Domain["api"] }/boards.json',
+                                        'thread_list':          '{ Domain["api"] }/{{0}}/threads.json',
+                                        'archived_thread_list': '{ Domain["api"] }/{{0}}/archive.json',
+                                        'catalog':              '{ Domain["api"] }/{{0}}/catalog.json'
+                                   }}");
+        }
+
+
+        private JObject Urls_get()
+        {
+            JObject retVal = JObject.FromObject(Template);
+            retVal.Add("domain", Domain);
+            retVal.Add("listing", Listing);
+
+            return retVal;
         }
     }
 }
